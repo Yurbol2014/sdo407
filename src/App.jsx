ddfefb51c3a06247936c3a960c713518c38b6996
Yuriy Bolotin <simplovg@gmail.com>
2021-05-28 01:10:03 +0300

    внес изменения компонент добавил

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,65 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-
+import { BrowserRouter, NavLink, Route } from 'react-router-dom';
+import Profile from "./components/Profile"
 import './App.css';
 
-const Profile = ()=>{
-   return <h1>Эта страница с настройками</h1>;
-}
+
+const Messages = ()=>{
+   return <h2>Эта   страница с сообщениями</h2>
+   
+   
+};
+const Settings = ()=>{
+   return <h2>Эта   страница с настройками</h2>
+   
+   
+};
+const Friends = ()=>{
+   return <h2>Друзья</h2>
+   
+   
+};
+const Menu = () =>{
+   return(
+   <div>
+   <div className="row">
+     <div className="col-sm-3">
+       <div className="nav flex-column nav-pills"aria-orientation="vertical">
+         <NavLink className="nav-link " to = "profile" >Профиль</NavLink>
+         <NavLink className="nav-link" to = "messages">Сообщение</NavLink>
+         <NavLink className="nav-link" to = "settings">Настройки</NavLink>
+         <NavLink className="nav-link" to = "friends" >Друзья</NavLink>
+       </div>
+     </div>
+     </div>
+     </div>
+     );
+
+};
 
 function App() {
   return (
-<div className="container-fluid">
-<div className="row">
-  <div className="col-sm-3">
-    <div className="nav flex-column nav-pills">
-      <a className="nav-link active" href = "profile" >Профиль</a>
-      <a className="nav-link" href = "messages">Сообщение</a>
-      <a className="nav-link" href = "settings">Настройки</a>
-      <a className="nav-link" href = "friends" >Друзья</a>
-    </div>
-  </div>
-   <div className="col-sm-9">
+   <div className="container-fluid">
    <BrowserRouter>
-   <Route path = "/profile" соmponent={Profile} />
+      <div className="row">
+
+      
+      <div className="col-sm-3">
+      <Menu/>
+      </div>
+
+   <div className='col-sm-9'>
+   
+   <Route path = '/profile' component={Profile} />
+   <Route path = '/messages' component={Messages} />
+   <Route path = '/settings' component={Settings} />
+   <Route path = '/friends' component={Friends} />
+   </div>
+   </div>
    </BrowserRouter>
    </div>
-</div>
-</div>
+
+
   );
 }
 
---
+++ src/components/Profile.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Profile = ()=>{
+   return <div className="row">
+      <div className="col-sm-3">
+         <img src="https://png.pngtree.com/png-vector/20190704/ourmid/pngtree-vector-user-young-boy-avatar-icon-png-image_1538408.jpg" alt=""width="100%" />
+      </div>
+      <div className="col-sm-9">
+         <h1>Фамилия имя</h1>
+         <h3> Обо мне</h3>
+         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit reiciendis nostrum praesentium, eaque veniam aspernatur numquam accusantium error pariatur blanditiis officiis architecto beatae repudiandae minus vel dolorem ipsum id. Laboriosam.z</p>
+      </div>
+   </div>
+
+};
+
+export default Profile;
